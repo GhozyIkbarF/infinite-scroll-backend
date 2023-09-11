520169fd5352f87e186108d46258f9a2c9d6e05f
GhozyIkbarF <ghozyikbarf@gmail.com>
2023-09-11 17:19:27 +0700

    docs: remoce comment

--- index.js
+++ index.js
@@ -4,16 +4,7 @@ import cors from 'cors';
 const app = express()
 import router from './router.js';
 
-// const allowedOrigins = ['https://infinite-scroll-taupe.vercel.app/', 'http://127.0.0.1:5500/index.html'];
-
-// const corsOptions = {
-//   origin: allowedOrigins,
-//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//   credentials: true,
-// };
-
 app.use(cors())
-// app.use(cors(corsOptions))
 app.use(express.json());
 app.use(router);
 
