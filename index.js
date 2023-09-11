fb4d754cb5b2f1f25264a3de9d28c6faa1daf8be
GhozyIkbarF <ghozyikbarf@gmail.com>
2023-09-11 17:10:02 +0700

    fix: fix middleware CORS

--- index.js
+++ index.js
@@ -4,16 +4,15 @@ import cors from 'cors';
 const app = express()
 import router from './router.js';
 
-app.use(function (req, res, next) {
+const allowedOrigins = ['https://infinite-scroll-taupe.vercel.app/', 'http://127.0.0.1:5500/index.html'];
 
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+const corsOptions = {
+  origin: allowedOrigins,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+};
 
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use(router);
 
